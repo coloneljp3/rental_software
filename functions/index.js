a195383b0c17aa109fb9ea1c8c4c3ddb522c6ab0
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2023-10-11 19:38:50 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,11 @@
+'use strict'
+var express = require('express');
+var app = express();
+var router = express.Router();
+var mysql = require('mysql2');
+var conn = mysql.createConnection(process.env.DATABASE_URL);
+var bodyParser = require('body-parser');
+var serverless = require('serverless-http');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
