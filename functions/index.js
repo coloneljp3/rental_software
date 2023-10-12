2115bef0f19a9aab88a699c57a97d4cfa7765139
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2023-10-11 19:50:12 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -8,4 +8,25 @@ var bodyParser = require('body-parser');
 var serverless = require('serverless-http');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+router.post('/',(req,res)=>{
+var user = req.body.user;
+var email_user = req.body.email_user;
+var email_psw = req.body.email_psw;
+if(user == "undefined"){
+res.send("")
+  
+}
+  else{
+res.send("")
+    
+  }
+})
 
+router.get('/',(req,res)=>{
+
+  
+})
+
+app.use('/.netlify/functions/write_account',router)
+
+module.exports.handler = serverless(app);
